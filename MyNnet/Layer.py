9058dd9fccabfc7e69a01bea8fc60e2824826034
Kartepolo <karte_polo@126.com>
2016-01-17 23:34:24 -0500

    Modifications

--- MyNnet/Layer.py
+++ MyNnet/Layer.py
@@ -12,20 +12,13 @@ class NNode:
         #Construct weights and bias arrays
         l = len(self.nodelist)
 
-def sigmoid(x):
-    def func(x):
-        return 1.0/(1 + np.exp(-x))
-    return func(x), func(x) * (1 - func(x))
 
 class NLayer:
     def __init__(self, size, ac_func, lam, sparsity_param, prevlayer = None):
         self.size = size
         self.func = ac_func
-        if prevlayer is not None:
-            self.weights = np.zeros(prevlayer.size(), size)
-            self.bias = np.zeros(size)
         self.prev = prevlayer
-        self.activations = None
+        self.activation = None
         self.initialized = False
         self.sample_size = 0
         self.lam = lam
@@ -34,27 +27,27 @@ class NLayer:
         self.deriative = 0
 
     def rand_init(self, bound = 1):
-        r = np.sqrt(6) / np.sqrt(self.size + self.prev.size() + 1) * bound
-        self.weights = np.random.random((self.prev.size(), self.size)) * 2 * r - r
+        r = np.sqrt(6) / np.sqrt(self.size + self.prev.size + 1) * bound
+        self.weights = np.random.random((self.prev.size, self.size)) * 2 * r - r
+        self.bias = np.zeros(self.size)
+        return np.concatenate((self.weights.reshape(self.size * self.prev.size), self.bias))
 
-    def size(self):
-        return self.size
 
     def cul_weight(self):
         return self.culmulative_weights
 
     def activate(self):
         if self.prev is not None:
-            prev_activations = self.prev.activate()
-            self.sample_size = prev_activations.shape[1]
-            z = prev_activations.dot(self.weights) + np.tile(self.bias, (self.sample_size, 1))
-            self.activations = self.func(z)
-            self.culmulative_weights = self.prev.cul_weight + np.sum(self.weights ** 2)
-        return self.activations
+            prev_activation = self.prev.activate()
+            self.sample_size = prev_activation.shape[0]
+            z = prev_activation.dot(self.weights) + np.tile(self.bias, (self.sample_size, 1))
+            self.activation, self.deriative = self.func(z)
+            self.culmulative_weights = self.prev.cul_weight() + np.sum(self.weights ** 2)
+        return self.activation
 
     def compute_gradient(self, delta):
-        w_gradient = self.activations.transpose().dot(delta) /  self.sample_size + self.lam * self.weights[1]
+        w_gradient = self.prev.activation.transpose().dot(delta) /  self.sample_size + self.lam * self.weights
         b_gradient = np.sum(delta, axis = 0).transpose() / self.sample_size
-        return np.concatenate((w_gradient.reshape(self.size * self.prev.size()), b_gradient))
+        return np.concatenate((w_gradient.reshape(self.size * self.prev.size), b_gradient))
 
 
--- MyNnet/NNet.py
+++ MyNnet/NNet.py
@@ -2,6 +2,10 @@ from Layer import NLayer
 import numpy as np
 from scipy.optimize import minimize
 
+def sigmoid(x):
+    def func(x):
+        return 1.0/(1 + np.exp(-x))
+    return func(x), func(x) * (1 - func(x))
 
 def kl_divergence(x, y):
     return x * np.log(x / y) + (1 - x) * np.log((1 - x) / (1 - y))
@@ -15,64 +19,83 @@ class NNet:
         prev = None
         for l in xrange(len(Layers)):
             self.Layers.append(NLayer(size = Layers[l], ac_func = func, prevlayer = prev, lam = lam, sparsity_param = sparsity_param))
-            prev = Layers[l]
+            prev = self.Layers[l]
         self.beta = beta
 
     def init(self):
+        result = []
         for l in self.Layers[1:]:
-            l.rand_init()
+            result.append(l.rand_init())
+        return np.hstack(result)
 
-    def feed_back(self, theta, data, debug):
-        self.Layers[0].activation = data
+
+    def forward_back(self, theta, debug = True):
         self.set_theta(theta)
-        LastLayer = self.Layers[len(self.Layers)]
+        LastLayer = self.Layers[len(self.Layers) - 1]
         result = LastLayer.activate()
         # Sparsity
-        rho_est = np.sum(LastLayer.prev.activation, axis = 0) / data.shape[0]
-        rho = np.tile(self.sparsity_param, LastLayer.size())
+        rho_est = np.sum(LastLayer.prev.activation, axis = 0) / LastLayer.sample_size
+        rho = np.tile(self.sparsity_param, LastLayer.prev.size)
         # Cost Function
-        cost = np.sum((result - data) ** 2) / (2 * LastLayer.size()) + \
-               self.lam * LastLayer.cul_weight() / len(self.Layers) + \
-               self.beta * np.sum(kl_divergence(rho_est, rho))
-        sparsity_delta = np.tile(- rho / rho_est + (1 - rho) / (1 - rho_est), (data.shape[0], 1))
+        cost = np.sum((result - self.output) ** 2) / (2 * LastLayer.sample_size) + \
+               self.lam * LastLayer.cul_weight() / (len(self.Layers) - 1) + \
+               self.beta * np.sum(kl_divergence(rho, rho_est))
+        sparsity_delta = np.tile(- rho / rho_est + (1 - rho) / (1 - rho_est), (LastLayer.sample_size, 1))
         if debug:
             return cost
         grad = []
-        delta = -(self.activations[0] - self.activations[2]) * Last
+        delta = -(self.output - LastLayer.activation) * LastLayer.deriative
         # Back_Propagation
-        for l in xrange(len(self.Layers), 0):
-            grad.insert(0, self.Layers[l].compute_gradient())
-        return grad
+        for l in xrange(len(self.Layers) - 1, 0, -1):
+            grad.insert(0, self.Layers[l].compute_gradient(delta))
+            delta = (delta.dot(self.Layers[l].weights.transpose())) * self.Layers[l - 1].deriative
+        return np.hstack(grad)
 
     def set_theta(self, theta):
         cur = 0
         for l in self.Layers[1:]:
-            cols = l.size()
-            rows = l.prev.size()
+            cols = l.size
+            rows = l.prev.size
             l.weights = theta[cur : (cur + cols * rows)].reshape(rows, cols)
             cur += cols * rows
             l.bias = theta[cur : (cur + cols)]
             cur += cols
 
-    def train(self, data):
+    def train(self, data, output, debug = True):
+        self.output = output
         theta = self.init()
-        options = {'maxiter': 500, 'disp': True}
-        J = lambda x: self.feed_back(theta = x, data = data)
-        result = minimize(J, theta, method='L-BFGS-B', jac=True, options=options)
-        opt_theta = result.x
-        print opt_theta
+        self.Layers[0].activation = data
+        if debug:
+            self.gradient_check(data, theta)
+        else:
+            options = {'maxiter': 500, 'disp': True}
+            J = lambda x: self.forward_back(theta = x)
+            result = minimize(J, theta, method='L-BFGS-B', jac=True, options=options)
+            opt_theta = result.x
+            print opt_theta
 
     def gradient_check(self, data, theta):
-        grad0 = self.feed_back(theta, data = data, debug = False)
-        epsilon = np.random.random(len(theta)) * 0.0001
+        grad0 = self.forward_back(theta, debug = False)
+        epsilon = 0.0001
         grad1 = np.zeros(theta.shape)
-        for i in theta:
-            c1 = self.feed_back(theta[i] + epsilon, data = data, debug = True)
-            c2 = self.feed_back(theta[i] - epsilon, data = data, debug = True)
-            grad1[i] = (c2 - c1)
+        for i in range(theta.shape[0]):
+            theta_epsilon_plus = np.array(theta, dtype=np.float64)
+            theta_epsilon_plus[i] = theta[i] + epsilon
+            theta_epsilon_minus = np.array(theta, dtype=np.float64)
+            theta_epsilon_minus[i] = theta[i] - epsilon
+            c1 = self.forward_back(theta_epsilon_plus, debug = True)
+            c2 = self.forward_back(theta_epsilon_minus, debug = True)
+            grad1[i] = (c1 - c2) / (2 * epsilon)
         diff = np.linalg.norm(grad1 - grad0) / np.linalg.norm(grad1 + grad0)
         print diff
 
+def test():
+    input_size = 64
+    data = np.random.randn(100, input_size)
+    ae = NNet(Layers = [64,36,64], func = sigmoid, lam = 3e-3, beta = 3, sparsity_param= 0.1)
+    ae.train(data,data)
+
+test()
 
 
 
